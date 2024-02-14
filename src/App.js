9d82f22ed0a3b2d65997bf5cae121bf783292649
jay-chatpalliwar <jaychatpalliwar1@outlook.com>
2024-02-14 07:49:39 +0530

    First Commit from this project.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import './App.css';
-import {Routes , Route} from 'react-router-dom'
-import NavBar from './components/NavBar';
-import NavBar2 from './components/NavBar2';
-import Feature from './components/Feature';
-import Footer from './components/Footer';
-
-function App() {
+import NavBar from './components/NavBar'
+import NavBar2 from './components/NavBar2'
+import Feature from './components/Feature'
+import Footer from './components/Footer'
+import './App.css'
+function App(){
   return (
-    <div className="App scroll-smooth relative">
-      <NavBar/>
-      <NavBar2/>
-      <Routes>
-        <Route path='/' element={<Feature/>}/>
-      </Routes>
-      <Footer/>
+    <div className='App flex flex-col relative'>
+    <NavBar/>
+    <NavBar2/>
+    <Feature/>
+    <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
